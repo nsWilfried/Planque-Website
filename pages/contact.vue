e323555470e9bb0686b189dd9fa919ad98b5bd44
root <root@localhost.localdomain>
2022-08-02 21:39:03 +0000

    Initialisation de l'application, définition des routes  et consommation de l'api sanity

---
+++ pages/contact.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+        <div v-for="post in posts" :key='post.id'>
+             {{post}}
+        </div>
+    </div>
+</template>
+
+<script>
+import {groq} from '@nuxtjs/sanity'
+export default {
+    async asyncData({isDev, route, store, env, params,$sanity, req, res, redirect, error}) {
+        const query = groq`*[_type=="post"]`
+        const posts = await $sanity.fetch(query)
+        return {
+            posts
+        }
+    },
+}
+</script>
