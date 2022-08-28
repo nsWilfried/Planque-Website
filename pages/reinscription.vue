ec3bd9c73a3c88eac55ca4013e3e6877da317ede
root <root@localhost.localdomain>
2022-08-28 03:06:38 +0000

    Reinscription page setup

--- pages/reinscription.vue
+++ pages/reinscription.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
     <Banner bannerTitle="Réinscription" />
+
+
+    <!-- formulaire de réinscription -->
+    <section class="bg-red-">
+      <div
+        class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12"
+      >
+        <div
+          style="width: 1320px"
+          class="mr-auto bg-green- place-self-center lg:col-span-7"
+        >
+
+
+          <div
+            class="lg:w-full lg:h-auto lg:flex lg:justify-center lg:items-center"
+          >
+          <inscription-form/>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <CTA/>
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
+import InscriptionForm from "../components/InscriptionForm.vue";
+import CTA from "../components/CTA.vue";
 export default {
   layout: "page",
-  components: { Banner },
+  components: { Banner, InscriptionForm, CTA },
 };
 </script>
